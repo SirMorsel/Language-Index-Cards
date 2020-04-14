268d4f80ee47717c3b8d304fe556c1cdc6bae3de
PATRICK_LAPTOP\Patrick <patrick.nibbia@gmail.com>
2020-04-14 15:04:15 +0200

    feat: add html and style components

---
+++ Index-Cards/src/app/_interface/index-card.ts
@@ -0,0 +1,5 @@
+export interface IndexCard {
+    id: number;
+    jap: string;
+    lat: string;
+}
--- Index-Cards/src/app/app.module.ts
+++ Index-Cards/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './page/card/card.component';
+import { HeaderComponent } from './page/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
